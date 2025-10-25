a05e8d7ca57f4122245a0cd57c957e09fb9108cb
Bruno <brunobb_88@yahoo.com.br>
2025-10-25 18:14:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 // ✅ 🔥 CORREÇÃO: CORS PARA PRODUÇÃO
 app.use((req, res, next) => {
